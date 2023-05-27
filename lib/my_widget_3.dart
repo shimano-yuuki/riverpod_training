107a0902ae5edb1a9158a6e2083171282e710d80
simanoyuki <yuukireirei5@gmail.com>
2023-05-27 16:29:00 +0900

    動画２と３の内容

---
+++ lib/my_widget_3.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MyWidget extends ConsumerWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    //WidgetRef refを追加する
+    return Text('ここにデータが表示されます');
+  }
+}
